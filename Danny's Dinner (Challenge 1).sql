e634c7a5f1aaaa47f6d7ee10306fa79280d838ce
Toluwalashe Ogunleye <121364075+Toluwaa-o@users.noreply.github.com>
2023-12-30 18:54:50 +0100

    Add files via upload

--- Danny's Dinner (Challenge 1).sql
+++ Danny's Dinner (Challenge 1).sql
@@ -1,3 +1,55 @@
+-- Setting up tables
+CREATE TABLE sales (
+  "customer_id" VARCHAR(1),
+  "order_date" DATE,
+  "product_id" INTEGER
+);
+
+INSERT INTO sales
+  ("customer_id", "order_date", "product_id")
+VALUES
+  ('A', '2021-01-01', '1'),
+  ('A', '2021-01-01', '2'),
+  ('A', '2021-01-07', '2'),
+  ('A', '2021-01-10', '3'),
+  ('A', '2021-01-11', '3'),
+  ('A', '2021-01-11', '3'),
+  ('B', '2021-01-01', '2'),
+  ('B', '2021-01-02', '2'),
+  ('B', '2021-01-04', '1'),
+  ('B', '2021-01-11', '1'),
+  ('B', '2021-01-16', '3'),
+  ('B', '2021-02-01', '3'),
+  ('C', '2021-01-01', '3'),
+  ('C', '2021-01-01', '3'),
+  ('C', '2021-01-07', '3');
+ 
+
+CREATE TABLE menu (
+  "product_id" INTEGER,
+  "product_name" VARCHAR(5),
+  "price" INTEGER
+);
+
+INSERT INTO menu
+  ("product_id", "product_name", "price")
+VALUES
+  ('1', 'sushi', '10'),
+  ('2', 'curry', '15'),
+  ('3', 'ramen', '12');
+  
+
+CREATE TABLE members (
+  "customer_id" VARCHAR(1),
+  "join_date" DATE
+);
+
+INSERT INTO members
+  ("customer_id", "join_date")
+VALUES
+  ('A', '2021-01-07'),
+  ('B', '2021-01-09');
+
 --What is the total amount each customer spent at the restaurant?
 SELECT customer_id, SUM(price) AS Amount_Spent
 FROM sales sal
@@ -164,7 +216,7 @@ SELECT sal.customer_id,
 FROM sales sal
 JOIN members mem ON sal.customer_id = mem.customer_id
 JOIN menu men ON sal.product_id = men.product_id
-WHERE order_date > join_date AND order_date < '2021-02-01'
+WHERE order_date >= join_date AND order_date < '2021-02-01'
 )
 
 SELECT customer_id, SUM(Points) as TotalPoints
